0c4b83fd5daaa4bf5572fe325a74554af5b283bc
José Tapadas Alves <jose@tapadas.dev>
2023-06-16 08:34:05 +0100

    Initial Commit

---
+++ Davar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Davar
+//
+//  Created by José Tapadas Alves on 16/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Davar/DavarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DavarApp.swift
+//  Davar
+//
+//  Created by José Tapadas Alves on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct DavarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
